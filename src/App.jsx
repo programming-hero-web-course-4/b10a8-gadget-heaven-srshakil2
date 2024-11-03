b75d7ae86f48a1c4d8b3bf2d18fd5573e6614568
srshakil2 <rh3642921@gmail.com>
2024-11-03 21:37:50 +0600

    setup-ready

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="read-the-docs text-5xl">
+        Click on the Vite and React logos to learn more
+      </p>
+      <button className="btn btn-accent text-xl">add deisyui</button>
+    </>
+  );
+}
+
+export default App;
